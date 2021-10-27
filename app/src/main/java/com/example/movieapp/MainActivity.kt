a36c8c38429aa35a36362743fd77ad8b62197160
SamuelNoye <samuel.noye@amalitech.org>
2021-10-27 15:41:29 -0700

    Backend login

--- app/src/main/java/com/example/movieapp/MainActivity.kt
+++ app/src/main/java/com/example/movieapp/MainActivity.kt
@@ -1,11 +1,123 @@
 package com.example.movieapp
 
+import android.app.ProgressDialog
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.text.TextUtils
+import android.util.Patterns
+import android.widget.Toast
+import androidx.appcompat.app.ActionBar
+import com.example.movieapp.databinding.ActivityMainBinding
+import com.google.firebase.auth.FirebaseAuth
+
 
 class MainActivity : AppCompatActivity() {
+
+    // View binding
+    private lateinit var binding: ActivityMainBinding
+
+    // ActionBAr
+    private lateinit var actionBar: ActionBar
+
+    // ProgressDialog
+    private lateinit var progressDialog: ProgressDialog
+
+    // FireBaseAuth
+    private lateinit var fireBAseAuth: FirebaseAuth
+
+    // global values
+    private var email = ""
+    private var password = ""
+
+
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        //configure actionbar
+        actionBar = supportActionBar!!
+        actionBar.title = "login"
+
+        //configure progress dialog
+        progressDialog = ProgressDialog(this)
+        progressDialog.setTitle("Please wait ")
+        progressDialog.setMessage("logging In...")
+        progressDialog.setCanceledOnTouchOutside(false)
+
+        //init fireBaseAuth
+        fireBAseAuth = FirebaseAuth.getInstance()
+        checkUser()
+
+        // handle click, open register activity
+        binding.noAccount.setOnClickListener {
+           // startActivity(Intent(this, signUpActivity::class.java))
+        }
+
+
+        // handle click, begin login
+        binding.loginBtn.setOnClickListener {
+            // before logging in, validate data
+            validateData()
+        }
+
     }
+
+    private fun validateData() {
+        //get Data
+        email = binding.emailField.text.toString().trim()
+        password = binding.passwordField.text.toString().trim()
+
+        //validate data
+        if(!Patterns.EMAIL_ADDRESS.matcher(email).matches()){
+            //invalid email format
+            binding.emailField.error = "Invalid email format"
+        }
+        else if(TextUtils.isEmpty(password)){
+            //password empty
+            binding.passwordField.error = "Please enter password"
+        }else{
+            //data is validated
+            fireBaseLogin()
+        }
+    }
+
+    private fun fireBaseLogin() {
+        // show progress
+        progressDialog.show()
+        fireBAseAuth.signInWithEmailAndPassword(email,password)
+            .addOnSuccessListener {
+                //login success
+                progressDialog.dismiss()
+
+                //get user info
+                val fireBaseUser = fireBAseAuth.currentUser
+                val email = fireBaseUser!!.email
+                Toast.makeText(this,"logged-In as $email", Toast.LENGTH_SHORT).show()
+
+                   //open profile
+              //  startActivity(Intent(this, ProfileActivity::class.java))
+                finish()
+
+            }
+            .addOnFailureListener { e->
+                // login failed
+                progressDialog.dismiss()
+                Toast.makeText(this,"login failed due to${e.message}", Toast.LENGTH_SHORT).show()
+            }
+    }
+
+    private fun checkUser() {
+        //if user is already logged in, goto profile activity
+        //get current user
+        val fireBaseUser = fireBAseAuth.currentUser
+        if(fireBaseUser != null){
+              //user is already logged in
+         //   startActivity(Intent(this, ProfileActivity::class.java))
+            finish()
+        }
+    }
+
 }
