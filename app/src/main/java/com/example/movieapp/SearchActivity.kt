8d7b240dc198ddabc16ae4c47ca8e934b7797bc7
SamuelNoye <samuel.noye@amalitech.org>
2021-11-04 10:34:08 -0700

    main

--- app/src/main/java/com/example/movieapp/SearchActivity.kt
+++ app/src/main/java/com/example/movieapp/SearchActivity.kt
@@ -3,9 +3,9 @@ package com.example.movieapp
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-class SearchActitvity : AppCompatActivity() {
+class SearchActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_search_actitvity)
+        setContentView(R.layout.activity_search_activity)
     }
 }
